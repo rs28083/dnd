e04c7a1a3b72edf25260e380008780d80f0135f4
Andrew Moose <amoose11@uncc.edu>
2017-04-10 17:20:29 +0000

    progress on login system

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -9,6 +9,15 @@ class RegisterController < ApplicationController
         @pw = params["password"]
         @pw2 = params["passwordC"]
         if @pw == @pw2 then
+           @hsh = PBKDF2.new do |p| 
+                p.password = @pw
+                p.salt = SecureRandom.base64(255)
+                p.iterations = 2
+            end
+            @hex = @hsh.hex_string
+            @character = User.create(username: @username, email: @email,password_hash: @hex, password_salt: @hsh.salt)
+            render 'show'
+            ##hsh.#bin_string
         else
             ##redirect / reload page ##session[:current_user_id] = @user.id
         end
